64efeee82ecee05f409b704435bfba89a0c2c250
joacosanchez12 <joaquin.sancheezz@gmail.com>
2024-10-31 01:46:03 +0000

    Finish

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,52 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
+import React, { useState } from "react";
 //create your first component
+
 const Home = () => {
+
+const [toDo,setToDo] = useState([]);
+const [inputValue, setInputValue] = useState('');
+
+function keyHandler(e) {
+	if (e.key==='Enter') {
+		if (e.target.value != ""){
+			setToDo(toDo.concat(inputValue));
+			e.target.value=""
+			setInputValue("")}
+						}
+				}
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<>
+			<h1 className="text-center">TodoListRF</h1>
+			<div className="m-auto tabs-borders">
+				<input className="m-4 ps-2" placeholder="What needs to be done?"
+				name="taskInput"
+				onChange={e => setInputValue(e.target.value)}
+				onKeyDown={ keyHandler }/>
+			</div>
+				
+					<ul className="list-group list-group-flush tabs-borders">
+						{toDo.map((task,indx)=>{
+							return (
+								
+									<li className="list-item d-flex justify-content-between mt-2 pb-2" key={indx}>
+										<span className="ps-4">{task}</span>
+										<button type="button" className="btn-close pe-5" aria-label="Close"
+										onClick={() => setToDo(toDo.filter((t,thisIndex) => indx != thisIndex)) }></button>
+									</li>								
+								
+									)
+						                     })
+						}
+											
+					</ul>
+			
+				<p>{toDo.length} {toDo.length==1 ? "item" : "items"} left</p>
+			
+			<div className="small-div" style={{width:"49%"}}></div>
+			<div className="small-div" style={{width:"48%"}}></div>
+		
+		</>
 	);
 };
 
